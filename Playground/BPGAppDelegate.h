ec23ecd399039b1d17eb0234997d3ded3facf929
Adrian Stabiszewski <as@grundid.de>
2014-03-25 15:49:47 +0100

    Initial Commit

---
+++ Playground/BPGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BPGAppDelegate.h
+//  Playground
+//
+//  Created by Adrian Stabiszewski on 25.03.14.
+//  Copyright (c) 2014 Adrian Stabiszewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Playground/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Playground
+//
+//  Created by Adrian Stabiszewski on 25.03.14.
+//  Copyright (c) 2014 Adrian Stabiszewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BPGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BPGAppDelegate class]));
+    }
+}
